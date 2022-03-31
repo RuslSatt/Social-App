ea4ad3f203549e897195f0b2634ce1591ccfa4d8
Ruslan Sattarov <r8sattarov@mail.ru>
2022-03-31 12:50:40 +0500

    feat: add navigation

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import HomePageStyle from './HomePage.module.css'
 import {Footer} from "../Footer/Footer";
 import {Search} from "../Search/Search";
+import {NavHomePage} from "./NavHomePage/NavHomePage";
 
 const HomePage = () => {
     return (
         <div className={HomePageStyle.HomePage}>
             <Search/>
+            <NavHomePage/>
             <Footer/>
         </div>
     );
--- src/components/HomePage/NavHomePage/NavHomePage.js
+++ src/components/HomePage/NavHomePage/NavHomePage.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import NavHomePageStyle from './NavHomePage.module.css'
+import {NavLink} from "react-router-dom";
 
 const NavHomePage = () => {
     return (
         <div className={NavHomePageStyle.NavHome}>
-
+            <NavLink className={`${NavHomePageStyle.button} ${NavHomePageStyle.active__button}`} to='/Popular'>
+                <button className={`${NavHomePageStyle.btn} ${NavHomePageStyle.active__btn}`}>
+                    Popular
+                </button>
+            </NavLink>
+            <NavLink className={NavHomePageStyle.button} to='/Popular'>
+                <button className={NavHomePageStyle.btn}>
+                    Trending
+                </button>
+            </NavLink>
+            <NavLink className={NavHomePageStyle.button} to='/Popular'>
+                <button className={NavHomePageStyle.btn}>
+                    Following
+                </button>
+            </NavLink>
         </div>
     );
 };
