6e9c42d1665e39436f8e96d7d692aa846c42e297
Ruslan Sattarov <r8sattarov@mail.ru>
2022-05-18 23:14:16 +0500

    feat: add formik for the signUp component

--- src/components/Auth/SignUp/FormSignUp.js
+++ src/components/Auth/SignUp/FormSignUp.js
@@ -1,112 +1,97 @@
-import React, { useRef } from "react";
+import React from "react";
 import SignInStyle from "../SignIn/SignIn.module.css";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-    createUser,
-    cleanForm,
-    updateFormSignUp,
-} from "../../../redux/AuthReducer";
+import { createUser, cleanForm } from "../../../redux/AuthReducer";
 import { Preload } from "../../Common/Preload/Preload";
+import { Formik, Form, Field } from "formik";
 
 const FormSignUp = () => {
-    const updateFormDispatch = useDispatch();
     const createUserDispatch = useDispatch();
     const cleanFormDispatch = useDispatch();
 
     const auth = useSelector((state) => state.auth);
 
-    let emailRef = useRef();
-    let passwordRef = useRef();
-    let confirmPasswordRef = useRef();
-
-    const callUpdateForm = () => {
-        updateFormDispatch(
-            updateFormSignUp(
-                emailRef.current.value,
-                passwordRef.current.value,
-                confirmPasswordRef.current.value,
-            ),
-        );
-    };
-    const callCreateUser = () => {
-        createUserDispatch(
-            createUser(
-                emailRef.current.value,
-                passwordRef.current.value,
-                confirmPasswordRef.current.value,
-            ),
-        );
+    const callCreateUser = (email, password, confirmPassword) => {
+        createUserDispatch(createUser(email, password, confirmPassword));
     };
     const callCleanForm = () => {
         cleanFormDispatch(cleanForm());
     };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
 
     return (
-        <form
-            onSubmit={handleSubmit}
-            className={`${SignInStyle.sign} + ${SignInStyle.signup}`}
-        >
-            {auth.error !== "" ? (
-                <div className={SignInStyle.error}>
-                    <span>{auth.error}</span>
-                </div>
-            ) : (
-                ""
-            )}
-            {auth.isPreload === true ? (
-                <div className={SignInStyle.preload}>
-                    <Preload />
-                </div>
-            ) : (
-                ""
-            )}
-            <div className={SignInStyle.sing__input}>
-                <input
-                    onChange={callUpdateForm}
-                    value={auth.userEmailUpdate}
-                    ref={emailRef}
-                    type="email"
-                    placeholder="Email"
-                ></input>
-            </div>
-            <div className={SignInStyle.sing__input}>
-                <input
-                    onChange={callUpdateForm}
-                    value={auth.userPasswordUpdate}
-                    ref={passwordRef}
-                    type="password"
-                    placeholder="Password"
-                ></input>
-            </div>
-            <div className={SignInStyle.sing__input}>
-                <input
-                    onChange={callUpdateForm}
-                    value={auth.confirmPasswordUpdate}
-                    ref={confirmPasswordRef}
-                    type="password"
-                    placeholder="Confirm Password"
-                ></input>
-            </div>
-            <div
-                className={`${SignInStyle.sing__log_in} + ${SignInStyle.sing__up}`}
+        <div className={`${SignInStyle.sign} + ${SignInStyle.signup}`}>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                    confirmPassword: "",
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    callCreateUser(
+                        values.email,
+                        values.password,
+                        values.confirmPassword,
+                    );
+                    setSubmitting(false);
+                }}
             >
-                <button disabled={auth.isPreload} onClick={callCreateUser}>
-                    SIGN UP
-                </button>
-            </div>
-            <p className={SignInStyle.other__yes_account}>
-                Already have account?
-                <span>
-                    <NavLink onClick={callCleanForm} to="/auth">
-                        SIGN IN
-                    </NavLink>
-                </span>
-            </p>
-        </form>
+                {({ isSubmitting }) => (
+                    <Form>
+                        {auth.error !== "" ? (
+                            <div className={SignInStyle.error}>
+                                <span>{auth.error}</span>
+                            </div>
+                        ) : (
+                            ""
+                        )}
+                        {auth.isPreload === true ? (
+                            <div className={SignInStyle.preload}>
+                                <Preload />
+                            </div>
+                        ) : (
+                            ""
+                        )}
+                        <div className={SignInStyle.sing__input}>
+                            <Field
+                                placeholder="Email"
+                                type="email"
+                                name="email"
+                            />
+                        </div>
+                        <div className={SignInStyle.sing__input}>
+                            <Field
+                                placeholder="Password"
+                                type="password"
+                                name="password"
+                            />
+                        </div>
+                        <div className={SignInStyle.sing__input}>
+                            <Field
+                                placeholder="Confirm password"
+                                type="password"
+                                name="confirmPassword"
+                            />
+                        </div>
+                        <div
+                            className={`${SignInStyle.sing__log_in} + ${SignInStyle.sing__up}`}
+                        >
+                            <button type="submit" disabled={isSubmitting}>
+                                SIGN UP
+                            </button>
+                        </div>
+                        <p className={SignInStyle.other__yes_account}>
+                            Already have account?
+                            <span>
+                                <NavLink onClick={callCleanForm} to="/auth">
+                                    SIGN IN
+                                </NavLink>
+                            </span>
+                        </p>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     );
 };
 
--- src/redux/AuthReducer.js
+++ src/redux/AuthReducer.js
@@ -1,169 +1,158 @@
-import {authApi} from "../API/API";
-import {auth} from '../data/firebase';
+import { authApi } from "../API/API";
+import { auth } from "../data/firebase";
 
-const ADD_FORM_SIGN_IN = 'ADD_FORM_SIGN_IN';
-const UPDATE_FORM_SIGN_IN = 'UPDATE_FORM_SIGN_IN';
-const ADD_FORM_SIGN_UP = 'ADD_FORM_SIGN_UP';
-const UPDATE_FORM_SIGN_UP = 'UPDATE_FORM_SIGN_UP';
-const ERROR_TYPE = 'ERROR_TYPE';
-const PRELOAD_TYPE = 'PRELOAD_TYPE';
-const REGISTER_TYPE = 'REGISTER_TYPE'
-const REMOVE_TYPE = 'REMOVE_TYPE';
-const GET_USER_ID = 'GET_USER_ID';
+const ADD_FORM_SIGN_IN = "ADD_FORM_SIGN_IN";
+const UPDATE_FORM_SIGN_IN = "UPDATE_FORM_SIGN_IN";
+const ADD_FORM_SIGN_UP = "ADD_FORM_SIGN_UP";
+const UPDATE_FORM_SIGN_UP = "UPDATE_FORM_SIGN_UP";
+const ERROR_TYPE = "ERROR_TYPE";
+const PRELOAD_TYPE = "PRELOAD_TYPE";
+const REGISTER_TYPE = "REGISTER_TYPE";
+const REMOVE_TYPE = "REMOVE_TYPE";
+const GET_USER_ID = "GET_USER_ID";
 
-const addFormSignIn = () => ({type: ADD_FORM_SIGN_IN,})
+const addFormSignIn = () => ({ type: ADD_FORM_SIGN_IN });
 
-const updateFormSignIn = (email, password) => ({type: UPDATE_FORM_SIGN_IN, email, password,})
+const updateFormSignIn = (email, password) => ({
+    type: UPDATE_FORM_SIGN_IN,
+    email,
+    password,
+});
 
-const addFormSignUp = () => ({type: ADD_FORM_SIGN_UP,})
+const addFormSignUp = () => ({ type: ADD_FORM_SIGN_UP });
 
 const updateFormSignUp = (email, password, confirmPassword) => ({
     type: UPDATE_FORM_SIGN_UP,
     email,
     password,
-    confirmPassword
-})
+    confirmPassword,
+});
 
-const errorAuth = (error) => ({type: ERROR_TYPE, error,})
+const errorAuth = (error) => ({ type: ERROR_TYPE, error });
 
-const preload = (value) => ({type: PRELOAD_TYPE, value})
+const preload = (value) => ({ type: PRELOAD_TYPE, value });
 
-const registerUser = (valueReg) => ({type: REGISTER_TYPE, valueReg})
+const registerUser = (valueReg) => ({ type: REGISTER_TYPE, valueReg });
 
-const cleanForm = () => ({type: REMOVE_TYPE,})
+const cleanForm = () => ({ type: REMOVE_TYPE });
 
-const getUserId = (id) => ({type: GET_USER_ID, id})
+const getUserId = (id) => ({ type: GET_USER_ID, id });
 
 let initialState = {
     userId: null,
-    userEmail: '',
-    userPassword: '',
-    confirmPassword: '',
-    userEmailUpdate: '',
-    userPasswordUpdate: '',
-    confirmPasswordUpdate: '',
     isRegister: false,
     isLogIn: false,
     isPreload: false,
-    error: '',
-}
+    error: "",
+};
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case REMOVE_TYPE: {
             return {
                 ...state,
-                userEmailUpdate: '',
-                userPasswordUpdate: '',
-                confirmPasswordUpdate: '',
-                error: '',
-            }
+                userEmailUpdate: "",
+                userPasswordUpdate: "",
+                confirmPasswordUpdate: "",
+                error: "",
+            };
         }
         case ADD_FORM_SIGN_IN: {
             return {
                 ...state,
                 userEmail: state.userEmailUpdate,
                 userPassword: state.userPasswordUpdate,
-                userEmailUpdate: '',
-                userPasswordUpdate: '',
-                error: '',
-            }
+                userEmailUpdate: "",
+                userPasswordUpdate: "",
+                error: "",
+            };
         }
         case UPDATE_FORM_SIGN_IN: {
             return {
                 ...state,
                 userEmailUpdate: action.email,
                 userPasswordUpdate: action.password,
-            }
-        }
-        case ADD_FORM_SIGN_UP: {
-            return {
-                ...state,
-                userEmail: state.userEmailUpdate,
-                userPassword: state.userPasswordUpdate,
-                confirmPassword: state.confirmPasswordUpdate,
-                userEmailUpdate: '',
-                userPasswordUpdate: '',
-                confirmPasswordUpdate: '',
-                error: '',
-            }
-        }
-        case UPDATE_FORM_SIGN_UP: {
-            return {
-                ...state,
-                userEmailUpdate: action.email,
-                userPasswordUpdate: action.password,
-                confirmPasswordUpdate: action.confirmPassword,
-            }
+            };
         }
         case PRELOAD_TYPE: {
             return {
                 ...state,
                 isPreload: action.value,
-            }
+            };
         }
         case REGISTER_TYPE: {
             return {
                 ...state,
                 isRegister: action.valueReg,
-            }
+            };
         }
         case GET_USER_ID: {
-            debugger
+            debugger;
             return {
                 ...state,
                 userId: action.id,
-            }
+            };
         }
         case ERROR_TYPE: {
             return {
                 ...state,
-                error: action.error
-            }
+                error: action.error,
+            };
         }
         default:
             return state;
     }
-}
+};
 
 const createUser = (email, password, passwordConfirm) => {
     return async (dispatch) => {
-        dispatch(preload(true))
+        dispatch(preload(true));
         if (password === passwordConfirm) {
-            await authApi.createUserDb(email, password).then(() => {
-                dispatch(addFormSignUp());
-                dispatch(preload(false));
-                dispatch(registerUser(true));
-            }).catch((error) => {
-                dispatch(errorAuth(error.message));
-                dispatch(preload(false));
-            });
+            await authApi
+                .createUserDb(email, password)
+                .then(() => {
+                    dispatch(preload(false));
+                    dispatch(registerUser(true));
+                })
+                .catch((error) => {
+                    dispatch(errorAuth(error.message));
+                    dispatch(preload(false));
+                });
         } else {
-            dispatch(errorAuth('Password is not right'));
+            dispatch(errorAuth("Password is not right"));
             dispatch(preload(false));
         }
-    }
-}
+    };
+};
 
 const signIn = (email, password) => {
     return async (dispatch) => {
-        dispatch(preload(true))
-        await authApi.signInDb(email, password).then(() => {
-            dispatch(addFormSignIn());
-            const user = auth.currentUser;
-            if (user) {
-                dispatch(getUserId(user.uid))
+        dispatch(preload(true));
+        await authApi
+            .signInDb(email, password)
+            .then(() => {
+                const user = auth.currentUser;
+                if (user) {
+                    dispatch(getUserId(user.uid));
+                    dispatch(preload(false));
+                } else {
+                    dispatch(errorAuth("No user is signed in."));
+                    dispatch(preload(false));
+                }
+            })
+            .catch(() => {
+                dispatch(errorAuth("Not right login or password"));
                 dispatch(preload(false));
-            } else {
-                dispatch(errorAuth('No user is signed in.'));
-                dispatch(preload(false));
-            }
-        }).catch(() => {
-            dispatch(errorAuth('Not right login or password'));
-            dispatch(preload(false));
-        });
+            });
+    };
+};
 
-    }
-}
-
-export {authReducer, updateFormSignUp, createUser, registerUser, cleanForm, updateFormSignIn, signIn};
+export {
+    authReducer,
+    updateFormSignUp,
+    createUser,
+    registerUser,
+    cleanForm,
+    updateFormSignIn,
+    signIn,
+};
