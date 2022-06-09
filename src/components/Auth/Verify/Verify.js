34f8a35d1227602d29bf82f024e9320c43e95105
Ruslan Sattarov <r8sattarov@mail.ru>
2022-06-09 20:52:40 +0500

    feat: add form

--- src/components/Auth/Verify/Verify.js
+++ src/components/Auth/Verify/Verify.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import Back from '../../../assets/images/auth-another-back.png'
 import Group from '../../../assets/images/group-other-auth.png'
 import { AuthHeader, AuthWrapper, Image, VerifyForm } from '../AuthStyles'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import { ButtonWrapper, InputWrapper } from '../../Style/StyleForm'
 
 const Verify = () => {
     return (
@@ -16,6 +18,35 @@ const Verify = () => {
                     A message with verification code was sent to your mobile
                     phone.
                 </Message>
+                <Formik
+                    initialValues={{
+                        code: '',
+                    }}
+                    onSubmit={(values) => {
+                        console.log(values)
+                    }}
+                >
+                    {() => (
+                        <Form>
+                            <InputWrapper>
+                                <Field
+                                    placeholder="Type verification code"
+                                    type="text"
+                                    name="code"
+                                />
+                                <ErrorMessage name="email" component="div" />
+                            </InputWrapper>
+
+                            <ButtonContainer>
+                                <button>Don't receive the code</button>
+                            </ButtonContainer>
+
+                            <ButtonWrapper>
+                                <button>VERIFY</button>
+                            </ButtonWrapper>
+                        </Form>
+                    )}
+                </Formik>
             </VerifyForm>
             <GroupImg>
                 <img src={Group} alt="group" />
@@ -41,13 +72,27 @@ const Message = styled.div`
     text-align: center;
     font-size: 15px;
     line-height: 150%;
+    margin-bottom: 40px;
 `
 
 const GroupImg = styled.div`
-    margin-top: auto;
     display: flex;
     align-items: center;
     justify-content: center;
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    button {
+        background: transparent;
+        line-height: 120%;
+        letter-spacing: 2px;
+        text-transform: uppercase;
+    }
 `
 
 export { Verify }
